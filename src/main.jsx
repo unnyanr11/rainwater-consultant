5e4dbee2cb8ef30a7fea16b720ec44dfddfd4c6d
Unnyan Rohatgi <unnyanr11@gmail.com>
2026-05-17 19:57:28 +0530

    fix: import admin.css in main.jsx to restore admin dashboard styles

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App'
 import './styles/tokens.css'
 import './styles/globals.css'
 import './styles/animations.css'
+import './styles/admin.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
